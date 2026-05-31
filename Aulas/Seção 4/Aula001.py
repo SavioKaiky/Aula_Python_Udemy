a0cc55bdea116b682e2cf5b9479f02bddf24b67e
SavioKaiky <saviokaiky03@protonmail.com>
2026-05-31 00:02:12 -0300

    Aula 001: introdução as funções

---
+++ Aulas/Seção 4/Aula001.py
@@ -0,0 +1,19 @@
+# def Print(a, b, c):
+#     print('Varias1')
+#     print('Varias2')
+#     print('Varias3')
+#     print('Varias4')
+
+# def imprimir(a, b, c):
+#     print(a, b, c)
+
+# imprimir(1, 2, 3)
+# imprimir(4, 5, 6)
+
+def saudacao(nome = 'Sem nome'):
+    print(f'Olá {nome}')
+
+saudacao('Savio Kaiky')
+saudacao('Maria')
+saudacao('Helena')
+saudacao()
