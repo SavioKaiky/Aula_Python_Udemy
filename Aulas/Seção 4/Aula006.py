97b3b895b7dce3e7ef5e6923d653724e1887f756
SavioKaiky <saviokaiky03@protonmail.com>
2026-06-01 23:12:46 -0300

    Aula 006: Argumento não nomeados (*args)

---
+++ Aulas/Seção 4/Aula006.py
@@ -0,0 +1,20 @@
+"""args - Argumentos não nomeados
+* - *args (empacotamento e desempacotamento)
+"""
+# lembre-te de desempacotamento
+
+x, y, *resto = 1, 2, 3, 4
+print(x, y, resto)
+
+# def soma(x, y):
+#     return x + y
+
+def soma(*args):
+    total = 0
+    for numero in args:
+        print(f"Total {total} {numero}")
+        total += numero
+        print(f'Total {total}')
+    print(total)
+
+soma(1, 2, 3, 4, 5, 6)
